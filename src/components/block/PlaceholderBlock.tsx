b5d1de47185c2077f9c363c15079ba3631b94004
Manas Chimpidi <manaschimpidi@gmail.com>
2023-04-26 04:20:05 -0500

    fix: format

--- src/components/block/PlaceholderBlock.tsx
+++ src/components/block/PlaceholderBlock.tsx
@@ -9,8 +9,8 @@ export type PlaceholderBlockProps = {
 
 export const PlaceholderBlock: FC<PlaceholderBlockProps> = ({name}) => (
 	<Container>
-		<Block isRounded color='grey'>
-			<Typography variant='h2' textAlign='center'>
+		<Block isRounded color="grey">
+			<Typography variant="h2" textAlign="center">
 				{name && `${name} Placeholder`}
 			</Typography>
 		</Block>
