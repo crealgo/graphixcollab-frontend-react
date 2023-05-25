5ce2bbde748100f59595f767af598c900ebc7ff8
Manas Chimpidi <manaschimpidi@gmail.com>
2023-05-25 14:03:34 -0500

    chore: resize images

--- scripts/resizeImages.js
+++ scripts/resizeImages.js
@@ -3,7 +3,7 @@ const sharp = require('sharp');
 const path = require('path');
 
 // const metaSizes = [16, 32, 100];
-const websiteSizes = [150, 250, 250, 1200, 1280, 1600, 1920];
+const websiteSizes = [150, 192, 250, 250, 512, 1200, 1280, 1600, 1920];
 
 // resize all images using sharp, excluding the ones in node_modules
 const resizeImages = async () => {
