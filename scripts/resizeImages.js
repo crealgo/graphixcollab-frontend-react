dcd2b4210dc3b9513513bc642cef01dabe976135
Manas Chimpidi <manaschimpidi@gmail.com>
2023-05-25 02:10:37 -0500

    perf: clean up script

--- scripts/resizeImages.js
+++ scripts/resizeImages.js
@@ -2,7 +2,7 @@ const {glob} = require('glob');
 const sharp = require('sharp');
 const path = require('path');
 
-const metaSizes = [16, 32, 100];
+// const metaSizes = [16, 32, 100];
 const websiteSizes = [150, 250, 250, 1200, 1280, 1600, 1920];
 
 // resize all images using sharp, excluding the ones in node_modules
@@ -10,7 +10,7 @@ const resizeImages = async () => {
 	const files = await glob(['public/**/*.{png,jpg,jpeg,svg}']);
 
 	files.forEach(async file => {
-		const {dir, name, ext} = path.parse(file);
+		const {dir, name} = path.parse(file);
 		const image = await sharp(file).metadata();
 
 		const constrainedSizes = websiteSizes.filter(
