8cf2466f24c581d9ddd14608c742c246f381afe0
Manas Chimpidi <manaschimpidi@gmail.com>
2023-07-19 21:37:48 -0500

    perf: remove extra console logs

--- src/forms/__utils__/getFormAction.tsx
+++ src/forms/__utils__/getFormAction.tsx
@@ -1,10 +1,6 @@
 export const getFormAction = (action: string): string => {
-	console.log({process})
-
 	const apiUrl = process.env.NEXT_PUBLIC_API_URL!;
 	const appId = process.env.NEXT_PUBLIC_APP_ID!;
-
-	console.log('getFormAction', `${apiUrl}/${appId}/${action}`);
 
 	return `${apiUrl}/${appId}/${action}`;
 };
