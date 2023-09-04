6c861ebfd82b2459ee7c0b90ea48e252ad567ee6
Manas Chimpidi <manaschimpidi@gmail.com>
2023-09-03 21:58:02 -0500

    fix: add timeline content

--- src/components/block/TimelineBlock.tsx
+++ src/components/block/TimelineBlock.tsx
@@ -21,6 +21,33 @@ const Content = styled(Container)`
 	}
 `;
 
+const milestones = [
+	{
+		stepNumber: 1,
+		title: 'Design',
+		image: 'assets/process/screen-printing/design-creation-min@512w.webp',
+		description: 'The design phase involves working with you to create custom print materials. We collaborate with you to understand your needs, design layouts, choose colors, and/or finalize the printing specifications.',
+	},
+	{
+		stepNumber: 2,
+		title: 'Procurement',
+		image: 'assets/process/screen-printing/prepared-screen-min@512w.webp',
+		description: 'We gather all the necessary printing materials and equipment. This includes buying ink, t-shirts, sashes, mugs, and other supplies needed for different print jobs.',
+	},
+	{
+		stepNumber: 3,
+		title: 'Product',
+		image: 'assets/process/screen-printing/prepared-screen-min@512w.webp',
+		description: 'Here we actually print the materials. We load the designs, set up the materials, and produce the printed products.',
+	},
+	{
+		stepNumber: 4,
+		title: 'Packing',
+		image: 'assets/process/screen-printing/prepared-screen-min@512w.webp',
+		description: 'Once the printing is done, we might trim, fold, or bind them based on client requirements. And then, finally package the finished prints, ensuring they are protected during transit or for delivery to clients. It\'s like preparing the printed materials for safe and attractive presentation to the clients or for shipping to their destinations.',
+	},
+];
+
 export const TimelineBlock: FC<TimelineBlockProps> = () => (
 	<Block className='EventBlock-root'>
 		<Content>
@@ -29,62 +56,9 @@ export const TimelineBlock: FC<TimelineBlockProps> = () => (
 			</Heading>
 		</Content>
 		<Timeline>
-			<Milestone
-				stepNumber={1}
-				title='Design'
-				description='Praesentium ipsam est officiis ad vero nobis odio sit. Enim illum rerum quam ut non aliquid. Sed provident et qui a in deserunt doloremque provident. Velit maxime esse dolorem. Est reprehenderit sit qui unde enim maxime incidunt ut.'
-				image='assets/process/screen-printing/design-creation-min@512w.webp'
-			/>
-			<Milestone
-				stepNumber={2}
-				title='Procurement'
-				description='Praesentium ipsam est officiis ad vero nobis odio sit. Enim illum rerum quam ut non aliquid. Sed provident et qui a in deserunt doloremque provident. Velit maxime esse dolorem. Est reprehenderit sit qui unde enim maxime incidunt ut.'
-				image='assets/process/screen-printing/prepared-screen-min@512w.webp'
-			/>
-			<Milestone
-				stepNumber={3}
-				title='Production'
-				description='Praesentium ipsam est officiis ad vero nobis odio sit. Enim illum rerum quam ut non aliquid. Sed provident et qui a in deserunt doloremque provident. Velit maxime esse dolorem. Est reprehenderit sit qui unde enim maxime incidunt ut.'
-				image='assets/process/screen-printing/screen-preparation-min@512w.webp'
-			/>
-			<Milestone
-				stepNumber={4}
-				title='Packing'
-				description='Praesentium ipsam est officiis ad vero nobis odio sit. Enim illum rerum quam ut non aliquid. Sed provident et qui a in deserunt doloremque provident. Velit maxime esse dolorem. Est reprehenderit sit qui unde enim maxime incidunt ut.'
-				image='assets/process/screen-printing/drying-and-curing-min@512w.webp'
-			/>
+			{milestones.map(milestone => (
+				<Milestone key={milestone.stepNumber} {...milestone}/>
+			))}
 		</Timeline>
 	</Block>
 );
-
-// OLD
-// export const TimelineBlock: FC<TimelineBlockProps> = () => {
-// 	const {breakpoints} = useTheme();
-// 	const isMobile = useMediaQuery(breakpoints.down('sm'));
-// 	const currentTimeline = 0;
-
-// 	const ResolvedEventComponent = isMobile
-// 		? MobileTimelineEvent
-// 		: TimelineEvent;
-
-// 	return (
-// 		<Block className='EventBlock-root'>
-// 			<Content>
-// 				<Heading isCentered level={2} className='mb-4'>
-// 					See how we <Mark color='magenta'>Screen Print</Mark>
-// 				</Heading>
-// 			</Content>
-// 			<Timeline>
-// 				{timelines[currentTimeline].steps.map((eventDetails, index) => (
-// 					<ResolvedEventComponent
-// 						key={index}
-// 						stepNumber={index + 1}
-// 						title={eventDetails.title}
-// 						description={eventDetails.description}
-// 						image={eventDetails.image}
-// 					/>
-// 				))}
-// 			</Timeline>
-// 		</Block>
-// 	);
-// };
