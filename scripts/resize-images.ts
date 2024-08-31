d01f8a6d70180e38d2274f893581b47116df2c84
Manas Chimpidi <manaschimpidi@gmail.com>
2024-08-31 16:23:38 -0500

    fix: update tsconfig, add settings, format

--- scripts/resize-images.ts
+++ scripts/resize-images.ts
@@ -21,54 +21,58 @@ const webpOptions: WebpOptions = {
 	effort: 6,
 };
 
-const filesToSearch = await glob('src/**/*.{json,ts,tsx}');
-// const sourceImages = await glob('src/**/*.{png,jpg,jpeg,avif,webp,gif,svg}');b
+async function main() {
+	const filesToSearch = await glob('src/**/*.{json,ts,tsx}');
+	// const sourceImages = await glob('src/**/*.{png,jpg,jpeg,avif,webp,gif,svg}');b
 
-const imageStringMatcher = /assets\/(?<imagePath>.*)-min@(?<imageWidth>.*)w.webp/gim;
+	const imageStringMatcher = /assets\/(?<imagePath>.*)-min@(?<imageWidth>.*)w.webp/gim;
 
-// loop through files with image strings
-// create array of images to resize
-// const imagesToResize: ImageToResize[] = [];
+	// loop through files with image strings
+	// create array of images to resize
+	// const imagesToResize: ImageToResize[] = [];
 
-for await (const file of filesToSearch) {
-	const matches = imageStringMatcher.exec(await fs.promises.readFile(file, 'utf8'));
+	for await (const file of filesToSearch) {
+		const matches = imageStringMatcher.exec(await fs.promises.readFile(file, 'utf8'));
 
-	if (!matches?.groups) {
-		continue;
-	}
+		if (!matches?.groups) {
+			continue;
+		}
 
-	const {imagePath, imageWidth} = matches.groups;
+		const {imagePath, imageWidth} = matches.groups;
 
-	// check if image has been resized already
-	if (fs.existsSync(`public/assets/${imagePath}-min@${imageWidth}w.webp`)) {
+		// check if image has been resized already
+		if (fs.existsSync(`public/assets/${imagePath}-min@${imageWidth}w.webp`)) {
 		// console.log(chalk.yellow(`${chalk.white(fileName)} has already been resized!`));
-		continue;
+			continue;
+		}
+
+		// find source file
+		const sourceFileSearch = await glob(`src/assets/${imagePath}.*`);
+
+		// check if source file exists
+		if (!sourceFileSearch.length) {
+			console.log(chalk.red(`${chalk.white(imagePath)} does not exist!`));
+			continue;
+		}
+
+		const sourceFile = sourceFileSearch[0];
+
+		let resizedImage = null;
+
+		if (imageWidth === 'og') {
+			resizedImage = await sharp(sourceFile, sharpOptions).webp(webpOptions).toBuffer();
+		} else {
+			resizedImage = await sharp(sourceFile, sharpOptions).resize(parseInt(imageWidth, 10)).webp(webpOptions).toBuffer();
+		}
+
+		const newFile = `public/assets/${imagePath}-min@${imageWidth}w.webp`;
+
+		await fs.promises.mkdir(path.dirname(newFile), {
+			recursive: true,
+		});
+
+		await fs.promises.writeFile(newFile, resizedImage);
 	}
-
-	// find source file
-	const sourceFileSearch = await glob(`src/assets/${imagePath}.*`);
-
-	// check if source file exists
-	if (!sourceFileSearch.length) {
-		console.log(chalk.red(`${chalk.white(imagePath)} does not exist!`));
-		continue;
-	}
-
-	const sourceFile = sourceFileSearch[0];
-
-	let resizedImage = null;
-
-	if (imageWidth === 'og') {
-		resizedImage = await sharp(sourceFile, sharpOptions).webp(webpOptions).toBuffer();
-	} else {
-		resizedImage = await sharp(sourceFile, sharpOptions).resize(parseInt(imageWidth, 10)).webp(webpOptions).toBuffer();
-	}
-
-	const newFile = `public/assets/${imagePath}-min@${imageWidth}w.webp`;
-
-	await fs.promises.mkdir(path.dirname(newFile), {
-		recursive: true,
-	});
-
-	await fs.promises.writeFile(newFile, resizedImage);
 }
+
+void main();
--- scripts/scrape-yelp-reviews.ts
+++ scripts/scrape-yelp-reviews.ts
@@ -2,41 +2,45 @@ import * as cheerio from 'cheerio';
 import fs from 'fs/promises';
 import Chance from 'chance';
 
-const chance = new Chance();
+async function main() {
+	const chance = new Chance();
 
-type Review = {
-	id: string;
-	name: string;
-	image: string | null;
-	text: string;
-};
+	type Review = {
+		id: string;
+		name: string;
+		image: string | null;
+		text: string;
+	};
 
-const urls = [
-	'https://www.yelp.com/biz/fashion-greek-usc-los-angeles?rr=5',
-	'https://www.yelp.com/biz/fashion-greek-usc-los-angeles?start=10&rr=5',
-	'https://www.yelp.com/biz/fashion-greek-usc-los-angeles?start=20&rr=5',
-];
+	const urls = [
+		'https://www.yelp.com/biz/fashion-greek-usc-los-angeles?rr=5',
+		'https://www.yelp.com/biz/fashion-greek-usc-los-angeles?start=10&rr=5',
+		'https://www.yelp.com/biz/fashion-greek-usc-los-angeles?start=20&rr=5',
+	];
 
-const reviews: Review[] = [];
+	const reviews: Review[] = [];
 
-for await (const url of urls) {
-	const response = await fetch(url);
-	const html = await response.text();
+	for await (const url of urls) {
+		const response = await fetch(url);
+		const html = await response.text();
 
-	const $ = cheerio.load(html);
+		const $ = cheerio.load(html);
 
-	const rawNames = $('#reviews ul li .user-passport-info').find('a');
-	const rawReviews = $('#reviews ul li').find('p[class^=comment]');
-	const rawImages = $('#reviews ul li a[href^=/user_detail]').find('img');
+		const rawNames = $('#reviews ul li .user-passport-info').find('a');
+		const rawReviews = $('#reviews ul li').find('p[class^=comment]');
+		const rawImages = $('#reviews ul li a[href^=/user_detail]').find('img');
 
-	for (let i = 0; i < rawNames.length; i++) {
-		reviews.push({
-			id: chance.guid({version: 4}),
-			name: $(rawNames[i]).text(),
-			image: $(rawImages[i]).attr('src') ?? null,
-			text: $(rawReviews[i]).text(),
-		});
+		for (let i = 0; i < rawNames.length; i++) {
+			reviews.push({
+				id: chance.guid({version: 4}),
+				name: $(rawNames[i]).text(),
+				image: $(rawImages[i]).attr('src') ?? null,
+				text: $(rawReviews[i]).text(),
+			});
+		}
 	}
+
+	await fs.writeFile('src/content/yelp-reviews.json', JSON.stringify(reviews, null, 2));
 }
 
-await fs.writeFile('src/content/yelp-reviews.json', JSON.stringify(reviews, null, 2));
+void main();
