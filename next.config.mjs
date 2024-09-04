ebfb4ac65ce7280e42e8a3b47b0fbd25e7db705b
Manas Chimpidi <manaschimpidi@gmail.com>
2024-09-03 20:49:34 -0500

    ci: upgrade staging deployment action

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,7 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-	webpack: config => {
-		config.module.rules.push({
-			test: /\.svg$/,
-			use: ['@svgr/webpack'],
-		});
-
-		// Unset client-side javascript that only works server-side
-		config.resolve.fallback = {fs: false, module: false, path: false};
-
-		return config;
-	},
+	output: 'export',
 };
 
 if (process.env.BASE_PATH) {
